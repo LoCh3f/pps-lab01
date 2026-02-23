2e1a868bab574d5a8b4b1953786482fa65ae5e16
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-02-23 14:55:35 +0100

    Refactor

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,20 +1,24 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
     private static final int WEAK_PIN = 1234;
+    private SmartDoorLock lock;
+    @BeforeEach
+    public void BeforeEach() {
+         lock = new SmartDoorLockImpl();
 
+    }
     @Test
     public void testSetUpLock() {
-        SmartDoorLock lock = new SmartDoorLockImpl();
         assertFalse(lock.isLocked());
     }
     @Test
     public void testLockUnlock() {
-        SmartDoorLock lock = new SmartDoorLockImpl();
         lock.lock();
         lock.unlock(SmartDoorLock.DEFAULT_PIN);
         assertFalse(lock.isLocked());
