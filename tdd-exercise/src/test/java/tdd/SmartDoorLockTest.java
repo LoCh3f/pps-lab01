115264a483e0e987793d94418e693d9d30c7a990
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-02-23 14:26:39 +0100

    Red light

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -6,9 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
     private static final int WEAK_PIN = 1234;
+
     @Test
     public void testSetUpLock() {
         SmartDoorLock lock = new SmartDoorLockImpl();
         assertFalse(lock.isLocked());
     }
+    @Test
+    public void testSetPin() {
+        SmartDoorLock lock = new SmartDoorLockImpl();
+        lock.setPin(WEAK_PIN);
+        lock.lock();
+        assertTrue(lock.isLocked());
+    }
+
 }
