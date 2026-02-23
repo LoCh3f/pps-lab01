d39925053c1a90ab8aea7d5235a9c99e7e2d3198
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-02-23 14:37:45 +0100

    Green Light

--- tdd-exercise/src/main/java/tdd/SmartDoorLockImpl.java
+++ tdd-exercise/src/main/java/tdd/SmartDoorLockImpl.java
@@ -2,12 +2,17 @@ package tdd;
 
 public class SmartDoorLockImpl implements SmartDoorLock {
     private boolean open;
+    private int pin = 0;
     public SmartDoorLockImpl() {
         this.open = true;
     }
     @Override
-    public void setPin(int pin) {
-
+    public void setPin(final int pin) throws IllegalStateException {
+       if (this.open) {
+           this.pin = pin;
+       } else {
+           throw new IllegalStateException("The Door is locked, cannot set up new pin");
+       }
     }
 
     @Override
@@ -17,7 +22,7 @@ public class SmartDoorLockImpl implements SmartDoorLock {
 
     @Override
     public void lock() {
-
+        this.open = false;
     }
 
     @Override
