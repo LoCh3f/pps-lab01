d7e1718e5acfdef070e3becf94510ba7e0bef279
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-02-23 14:23:09 +0100

    Green Light

---
+++ tdd-exercise/src/main/java/tdd/SmartDoorLockImpl.java
@@ -0,0 +1,47 @@
+package tdd;
+
+public class SmartDoorLockImpl implements SmartDoorLock {
+    private boolean open;
+    public SmartDoorLockImpl() {
+        this.open = true;
+    }
+    @Override
+    public void setPin(int pin) {
+
+    }
+
+    @Override
+    public void unlock(int pin) {
+
+    }
+
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public boolean isLocked() {
+        return !this.open;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return false;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return 0;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+    private static final int WEAK_PIN = 1234;
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testSetUpLock() {
+        SmartDoorLock lock = new SmartDoorLockImpl();
+        assertFalse(lock.isLocked());
     }
 }
