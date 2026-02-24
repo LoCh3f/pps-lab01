6956f29ae92ef8bc015a07eaec6258b7828a68e8
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-02-24 15:20:52 +0100

    Green Light

--- tdd-exercise/src/main/java/tdd/stack/MinMaxStackImpl.java
+++ tdd-exercise/src/main/java/tdd/stack/MinMaxStackImpl.java
@@ -1,14 +1,40 @@
 package tdd.stack;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 public class MinMaxStackImpl implements MinMaxStack {
+
+    private final Stack<Integer> stack;
+    private record MinMax(int min, int max) {}
+    private final List<MinMax> minMaxValues;
+
+    public MinMaxStackImpl() {
+        this.stack = new Stack<>();
+        this.minMaxValues = new LinkedList<>();
+    }
     @Override
-    public void push(int value) {
+    public void push(final int value) {
+        if(this.minMaxValues.isEmpty()){
+            this.minMaxValues.add(new MinMax(value,value));
+        } else if (lastMinMaxPair().min > value) {
+            this.minMaxValues.add(new MinMax(value, lastMinMaxPair().max));
+        } else if (lastMinMaxPair().max < value ) {
+            this.minMaxValues.add(new MinMax(lastMinMaxPair().min,value));
+        } else {
+            this.minMaxValues.add(lastMinMaxPair());
+        }
+        stack.push(value);
+
 
     }
 
     @Override
     public int pop() {
-        return 0;
+        this.minMaxValues.remove(this.lastMinMaxPair());
+        return stack.pop();
     }
 
     @Override
@@ -34,5 +60,9 @@ public class MinMaxStackImpl implements MinMaxStack {
     @Override
     public int size() {
         return 0;
+    }
+
+    private MinMax lastMinMaxPair() {
+        return this.minMaxValues.get(this.minMaxValues.size()-1);
     }
 }
