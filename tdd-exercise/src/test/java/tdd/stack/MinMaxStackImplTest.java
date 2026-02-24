7e819fbca1142a48c67b83529893c63f18764d7b
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-02-24 14:43:30 +0100

    Red light

--- tdd-exercise/src/test/java/tdd/stack/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/stack/MinMaxStackImplTest.java
@@ -1,12 +1,20 @@
 package tdd.stack;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
+    private static final int FIRST_ELEMENT = 1;
+    private MinMaxStack stack;
+    @BeforeEach
+    void BeforeEach() {
+        this.stack = new MinMaxStackImpl();
+    }
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testPushPop() {
+        stack.push(FIRST_ELEMENT);
+        assertEquals(FIRST_ELEMENT, this.stack.pop());
     }
 }
